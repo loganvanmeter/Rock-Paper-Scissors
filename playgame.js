fb454bd52ddb39c9a96ebe8fcc714315e3ce6dec
Logan Van Meter <vanmeterlj@gmail.com>
2022-06-29 15:37:29 -0500

    Added computer opponent logic

--- playgame.js
+++ playgame.js
@@ -1,1 +1,19 @@
 "use strict";
+const genRandomNumber = function () {
+	return Math.floor(Math.random() * 4);
+};
+
+const computerPlay = function () {
+	let randomNum = genRandomNumber();
+	let answer = "";
+	if (randomNum === 1) {
+		answer = "Rock";
+	} else if (randomNum === 2) {
+		answer = "Paper";
+	} else {
+		answer = "Scissors";
+	}
+	return answer;
+};
+
+console.log(computerPlay());
